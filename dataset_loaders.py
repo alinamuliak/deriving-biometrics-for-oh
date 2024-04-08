96b69d296abe1d2af804b2c2061cff2ae2f8eb78
alinamuliak <alina.muliakk@gmail.com>
2024-04-08 23:07:41 +0300

    fixed data loading for cnn

--- dataset_loaders.py
+++ dataset_loaders.py
@@ -126,3 +126,59 @@ def create_dataloaders(X: list, y: list, batch_size: int, seed: int = 7777777,
         return train_loader, val_loader, test_loader, class_weights_tensor
 
     return train_loader, val_loader, test_loader
+
+
+def create_cnn_dataloaders(X: list, y: list, batch_size: int, seed: int = 7777777,
+                           return_class_weights: bool = True, verbose: bool = False)\
+        -> tuple[DataLoader, DataLoader, DataLoader] | tuple[DataLoader, DataLoader, DataLoader, torch.FloatTensor]:
+    """
+    Splits the data for train, validation, and test datasets.
+    Creates dataloaders for each with the specified batch_size.
+
+    :param X: List of sequences of signals.
+    :param y: List of labels.
+    :param batch_size: Batch size for dataloaders.
+    :param seed: Random seed for data splitting and shuffling.
+    :param return_class_weights: Whether to return an array with class weights from train dataset. Defaults to True.
+    :param verbose: Whether to print progress during dataloaders creation.
+
+    :return: Train, validation, and test dataloaders. If return_class_weights is True, returns this array, too.
+    """
+    generator = torch.Generator().manual_seed(seed)
+    X_expanded, y_expanded = [], []
+    for signal, labels in zip(X, y):
+        for window, label in zip(signal, labels):
+            window = np.array(window).T
+            X_expanded.append(window)
+            y_expanded.append(label)
+
+    X_train, X_val, y_train, y_val = train_test_split(X_expanded, y_expanded, test_size=0.175, random_state=seed)
+    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.175, random_state=seed)
+
+    if verbose:
+        print("Created train, validation, and test datasets.")
+        print(f"X_train length: {len(X_train)}, X_val length: {len(X_val)}, and X_test length: {len(X_test)}.")
+
+    train_dataset = SequenceDataset(X_train, y_train)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+                              generator=generator)
+
+    val_dataset = SequenceDataset(X_val, y_val)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False,
+                            generator=generator)
+
+    test_dataset = SequenceDataset(X_test, y_test, test_dataset=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
+                             generator=generator)
+
+    if verbose:
+        print(f"Created dataloaders with batch size {batch_size}.")
+
+    if return_class_weights:
+        class_counts = np.bincount(y_train)
+        class_weights = class_counts.sum() / (len(class_counts) * class_counts)
+        class_weights_tensor = torch.FloatTensor(class_weights)
+
+        return train_loader, val_loader, test_loader, class_weights_tensor
+
+    return train_loader, val_loader, test_loader
